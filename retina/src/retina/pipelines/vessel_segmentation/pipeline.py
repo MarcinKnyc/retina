25ee73583bcdc9094a20cf11f877c30bcf5e18f0
Marcin Knyc <maroxesen@gmail.com>
2024-05-23 16:50:45 +0200

    feat: the new pipeline now works. Returns around accuracy 0.8. It is adapted to kedro and kedro run and viz work

--- retina/src/retina/pipelines/vessel_segmentation/pipeline.py
+++ retina/src/retina/pipelines/vessel_segmentation/pipeline.py
@@ -1,3 +1,5 @@
+import logging
+from tqdm import tqdm
 import cv2
 import os
 import matplotlib.pyplot as plt
@@ -9,27 +11,23 @@ from sklearn.neighbors import KNeighborsClassifier
 from skimage.util import view_as_windows
 
 
-def load_images(image_dir: str, mask_dir: str) -> tuple:
-    """Utility function to load and preprocess images and masks."""
-    photo_paths = [os.path.join(image_dir, x) for x in os.listdir(image_dir)]
-    label_paths = [os.path.join(mask_dir, x) for x in os.listdir(mask_dir)]
-    
-    raw_photos = [cv2.cvtColor(cv2.imread(str(path)), cv2.COLOR_BGR2RGB) for path in photo_paths]
-    masks = [cv2.cvtColor(cv2.imread(str(path)), cv2.COLOR_BGR2GRAY) for path in label_paths]
-    masks = [np.where(mask > 0, 255, 0) for mask in masks]
-    
-    return raw_photos, masks
-
 def load_data(input_path: str) -> tuple:
     """Loads and preprocesses the train and test datasets."""
-    train_image_dir = os.path.join(input_path, 'train/image')
-    train_mask_dir = os.path.join(input_path, 'train/mask')
-    test_image_dir = os.path.join(input_path, 'test/image')
-    test_mask_dir = os.path.join(input_path, 'test/mask')
-    
-    train_raw_photos, train_masks = load_images(train_image_dir, train_mask_dir)
-    test_raw_photos, test_masks = load_images(test_image_dir, test_mask_dir)
-    
+    train_photo_paths = [os.path.join(input_path, 'train/image', x) for x in os.listdir(os.path.join(input_path, 'train/image'))]
+    train_label_paths = [os.path.join(input_path, 'train/mask', x) for x in os.listdir(os.path.join(input_path, 'train/mask'))]
+
+    train_raw_photos = [cv2.cvtColor(cv2.imread(str(path)), cv2.COLOR_BGR2RGB) for path in train_photo_paths]
+    train_masks = [cv2.cvtColor(cv2.imread(str(path)), cv2.COLOR_BGR2GRAY) for path in train_label_paths]
+    train_masks = [np.where(mask > 0, 255, 0) for mask in train_masks]
+
+    test_photo_paths = [os.path.join(input_path, 'test/image', x) for x in os.listdir(os.path.join(input_path, 'test/image'))]
+    test_label_paths = [os.path.join(input_path, 'test/mask', x) for x in os.listdir(os.path.join(input_path, 'test/mask'))]
+
+    test_raw_photos = [cv2.cvtColor(cv2.imread(str(path)), cv2.COLOR_BGR2RGB) for path in test_photo_paths]
+    test_masks = [cv2.cvtColor(cv2.imread(str(path)), cv2.COLOR_BGR2GRAY) for path in test_label_paths]
+    test_masks = np.array(test_masks).astype(np.uint8)
+    test_masks = [np.where(mask > 0, 255, 0) for mask in test_masks]
+
     return train_raw_photos, train_masks, test_raw_photos, test_masks
 
 
@@ -46,8 +44,9 @@ def preprocess_images(train_raw_photos: list, test_raw_photos: list) -> tuple:
     return train_photos_clahe, test_photos
 
 
-def plot_images(images_set: list, title: str, output_path: str, filename: str, figsize=(20, 10), cmap=None):
+def plot_images(train_raw_photos, train_masks, title: str, output_path: str, filename: str, figsize=(20, 10), cmap=None):
     """Plots images in a grid layout and saves the plot to a file."""
+    images_set = [train_raw_photos[:5], train_masks[:5]]
     plt.figure(figsize=figsize)
     plt.suptitle(title, fontsize=20)
     for i in range(len(images_set[0])):
@@ -77,7 +76,7 @@ def create_dataset(photos: list, masks: list, size: int = 5, step: int = 1) -> t
     """Creates a dataset by extracting patches and features from images."""
     ds_photos = []
     ds_masks = []
-    for photo, mask in zip(photos, masks):
+    for photo, mask in tqdm(zip(photos, masks), total=len(photos), desc='Processing photos and their masks'):
         photo_pad = np.pad(photo, ((size // 2, size // 2), (size // 2, size // 2)), mode='constant')
         mask_pad = np.pad(mask, ((size // 2, size // 2), (size // 2, size // 2)), mode='constant')
 
@@ -109,7 +108,6 @@ def train_model(train_features: list, train_labels: list, output_path: str) -> K
     """Trains a K-Nearest Neighbors classifier and saves it."""
     knn_classifier = KNeighborsClassifier(n_neighbors=5)
     knn_classifier.fit(train_features, train_labels)
-    pickle.dump(knn_classifier, open(os.path.join(output_path, 'knnclassifier.pth'), 'wb'))
 
     return knn_classifier
 
@@ -158,15 +156,15 @@ def get_quality(masks: list, postprocessed_masks: list) -> tuple:
     return accuracy_scores, sensitivity_scores, specificity_scores
 
 
-def plot_results(images_set: list, title: str, test_raw_photos: list, test_masks: list, output_path: str, filename: str, figsize=(27, 10), n=10):
+def plot_results(test_raw_photos: list, test_masks: list, y_pred_images, result_plot_title: str, output_path: str, filename: str, figsize=(27, 10), n=10):
     """Plots the results along with accuracy, sensitivity, and specificity metrics, and saves the plot to a file."""
-    accuracy_scores, sensitivity_scores, specificity_scores = get_quality(images_set[1], images_set[2])
+    accuracy_scores, sensitivity_scores, specificity_scores = get_quality(test_masks, y_pred_images)
 
-    if n > len(images_set[0]):
-        n = len(images_set[0])
+    if n > len(test_raw_photos):
+        n = len(test_raw_photos)
 
     plt.figure(figsize=figsize)
-    plt.suptitle(title, fontsize=20)
+    plt.suptitle(result_plot_title, fontsize=20)
     for i in range(n):
         plt.subplot(3, 10, i + 1)
         plt.imshow(test_raw_photos[i], cmap='gray')
@@ -177,7 +175,7 @@ def plot_results(images_set: list, title: str, test_raw_photos: list, test_masks
         plt.axis('off')
         plt.title('Ground truth')
         plt.subplot(3, 10, i + 21)
-        plt.imshow(images_set[2][i], cmap='gray')
+        plt.imshow(y_pred_images[i], cmap='gray')
         plt.axis('off')
         plt.title('Mask\nAcc: {:.3f}\nSens: {:.3f}\nSpec: {:.3f}'.format(accuracy_scores[i], sensitivity_scores[i], specificity_scores[i]))
         plt.tight_layout()
@@ -192,22 +190,59 @@ def save_plot(output_path: str, filename: str):
     plt.savefig(os.path.join(output_path, filename))
     plt.close()
 
-if __name__ == '__main__':
-    input_path = '/kaggle/input/retina-blood-vessel/Data/'
-    output_path = '/kaggle/working/'
 
-    train_raw_photos, train_masks, test_raw_photos, test_masks = load_data(input_path)
-    train_photos, test_photos = preprocess_images(train_raw_photos, test_raw_photos)
-    
-    plot_images([train_raw_photos[:5], train_masks[:5]], 'Image and Mask', output_path, 'image_and_mask.png')
+from kedro.pipeline import Pipeline, node
 
-    train_features, train_labels = create_dataset(train_photos, train_masks, size=5, step=5)
-    train_features_under, train_labels_under = undersampling(train_features, train_labels)
-
-    print(train_labels_under.count(0), train_labels_under.count(255))
-
-    knn_classifier = train_model(train_features_under, train_labels_under, output_path)
-
-    y_pred_images = predict_model(knn_classifier, test_photos, test_masks)
-
-    plot_results([test_photos, test_masks, y_pred_images], 'Ground truth and predicted mask', test_raw_photos, test_masks, output_path, 'results.png')
+def create_pipeline(**kwargs):
+    return Pipeline(
+        [
+            node(
+                func=load_data,
+                inputs="params:input_path",
+                outputs=["train_raw_photos", "train_masks", "test_raw_photos", "test_masks"],
+                name="load_data_node",
+            ),
+            node(
+                func=preprocess_images,
+                inputs=["train_raw_photos", "test_raw_photos"],
+                outputs=["train_photos", "test_photos"],
+                name="preprocess_images_node",
+            ),
+            node(
+                func=plot_images,
+                inputs=["train_raw_photos", "train_masks", "params:image_plot_title", "params:output_path", "params:image_plot_filename"],
+                outputs=None,
+                name="plot_images_node",
+            ),
+            node(
+                func=create_dataset,
+                inputs=["train_photos", "train_masks", "params:dataset_size", "params:dataset_step"],
+                outputs=["train_features", "train_labels"],
+                name="create_dataset_node",
+            ),
+            node(
+                func=undersampling,
+                inputs=["train_features", "train_labels"],
+                outputs=["train_features_under", "train_labels_under"],
+                name="undersampling_node",
+            ),
+            node(
+                func=train_model,
+                inputs=["train_features_under", "train_labels_under", "params:output_path"],
+                outputs="knn_classifier",
+                name="train_model_node",
+            ),
+            node(
+                func=predict_model,
+                inputs=["knn_classifier", "test_photos", "test_masks"],
+                outputs="y_pred_images",
+                name="predict_model_node",
+            ),
+            node(
+                func=plot_results,
+                inputs=["test_photos", "test_masks", "y_pred_images", "params:result_plot_title", "params:output_path", "params:result_plot_filename"],                
+                outputs=None,
+                name="plot_results_node",
+            ),
+        ]
+    )
