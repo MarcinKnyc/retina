046122f12605504408b17b6aa7893306dd06dba9
xrev_v <xrevco@gmail.com>
2024-06-04 23:41:20 +0200

    refactor: removed dead code

--- retina/src/retina/pipelines/vessel_segmentation/file_operations.py
+++ retina/src/retina/pipelines/vessel_segmentation/file_operations.py
@@ -47,31 +47,6 @@ def load_data(input_path: str, debug: bool) -> tuple:
     return train_photos, train_masks, test_photos, test_masks
 
 
-def rename_folder(old_path: str, new_path: str) -> None:
-    """
-    Renames a folder from old_path to new_path.
-
-    Parameters:
-    old_path (str): The current path of the folder.
-    new_path (str): The new path of the folder.
-
-    Returns:
-    None
-    """
-    try:
-        os.rename(old_path, new_path)
-        print(f"Folder renamed from '{old_path}' to '{
-              new_path}' successfully.")
-    except FileNotFoundError:
-        print(f"The folder at '{old_path}' does not exist.")
-    except FileExistsError:
-        print(f"A folder already exists at the new path '{new_path}'.")
-    except PermissionError:
-        print(f"Permission denied to rename the folder.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-
 def unzip(file: str, output: str):
     with zipfile.ZipFile(file, 'r') as zip_ref:
         zip_ref.extractall(output)
