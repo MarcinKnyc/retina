a800a2d20d105a6034358dc0bee0de3d04ded444
xrev_v <xrevco@gmail.com>
2024-06-16 17:18:05 +0200

    fix: `morphological_top_hat()` + style

--- retina/src/retina/pipelines/vessel_segmentation/feature_extraction.py
+++ retina/src/retina/pipelines/vessel_segmentation/feature_extraction.py
@@ -38,24 +38,35 @@ def compute_gradient_orientation(image):
 
 
 def morphological_top_hat(image):
+    def rotate(image, angle):
+        size = image.shape[1::-1]
+        image_center = tuple(np.array(size) / 2)
+        rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+        return cv2.warpAffine(image, rot_mat, size)
+
     se_length = 21
-    top_hat = np.zeros_like(image, dtype=np.float64)
+    se = cv2.getStructuringElement(cv2.MORPH_RECT, (se_length, 1))
+    se = cv2.copyMakeBorder(
+        se, se_length//2, se_length//2, 0, 0, cv2.BORDER_CONSTANT, 0)
+
+    top_hat = []
     for angle in range(0, 180, 22):
-        se = cv2.getStructuringElement(cv2.MORPH_RECT, (se_length, 1))
-        rotated_se = cv2.warpAffine(se, cv2.getRotationMatrix2D(
-            (se_length//2, 0), angle, 1), (se_length, se_length))
-        top_hat += cv2.morphologyEx(image, cv2.MORPH_TOPHAT, rotated_se)
-    return top_hat
+        rotated_se = rotate(se, angle)
+        top_hat.append(cv2.morphologyEx(image, cv2.MORPH_TOPHAT, rotated_se))
+
+    return sum(top_hat)
 
 
 def line_strength(image):
     lines = np.zeros_like(image, dtype=np.float64)
     se_length = 15
+
     for angle in range(0, 180, 15):
         se = cv2.getStructuringElement(cv2.MORPH_RECT, (se_length, 1))
         rotated_se = cv2.warpAffine(se, cv2.getRotationMatrix2D(
             (se_length//2, 0), angle, 1), (se_length, se_length))
         lines = np.maximum(lines, cv2.filter2D(image, cv2.CV_64F, rotated_se))
+
     return lines
 
 
@@ -65,30 +76,29 @@ def gabor_filter_responses(image):
         gabor_features = []
         for theta in range(0, 180, 10):
             theta_rad = theta * np.pi / 180
-        
             gabor_kernel = cv2.getGaborKernel(
                 (sigma*6, sigma*6), sigma, theta_rad, 10.0, 0.5, 0, ktype=cv2.CV_64F)
             gabor_features.append(cv2.filter2D(
                 image, cv2.CV_64F, gabor_kernel))
         final_features.append(np.stack(gabor_features, axis=-1).max(axis=-1))
 
-    # import code
-    # code.interact(local=locals())
     return final_features
 
-def extract_features( photos ):
+
+def extract_features(photos):
     feature_photos = []
     for photo in tqdm(photos, total=len(photos), desc='Extracting features'):
         feature_vector = create_feature_vector(photo)
         feature_photos.append(feature_vector)
-    
+
     return feature_photos
 
-def create_dataset( features, masks ):
+
+def create_dataset(features, masks):
     ds_photos = []
     ds_masks = []
-    for feature_vector,mask in zip(features,masks):
-        height, width, num_features = feature_vector.shape
+    for feature_vector, mask in zip(features, masks):
+        _, _, num_features = feature_vector.shape
         # Flatten the features
         flattened_features = feature_vector.reshape(-1, num_features)
         ds_photos.append(flattened_features)
